3c379ee5e494e8043792741d64fd08d6f93917e1
yb2k <115099737+JackTallis@users.noreply.github.com>
2025-07-07 21:50:11 +0100

    Statement & Variables
    The beginning of the theory

--- beginning.cpp
+++ beginning.cpp
@@ -3,5 +3,7 @@
 int main()
 {
     std::cout << "Here is some text.";
+                                        //This cpp is just for a basic print statement
+    std::cout << "Hi, how are you?";
     return 0;
 }
---
+++ statementandvariables.cpp
@@ -0,0 +1,13 @@
+#include <iostream> // for std::cout
+
+int main()
+{
+    std::cout << 5;       // print the literal number `5`
+    std::cout << -6.7;    // print the literal number `-6.7`
+    std::cout << 'H';     // print the literal character `H`
+    std::cout << "Hello"; // print the literal text `Hello`
+
+    return 0;
+}
+
+// If a character value or text value is not properly quoted, the compiler will try to interpret that value as if it were C++ code. That will almost always result in a compilation error.
